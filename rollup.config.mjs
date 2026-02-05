0e487b2a6428084d74a4231f75ad99af9ae6f314
MarshallRdgz <marshalltankian@gmail.com>
2026-02-05 01:22:38 -0600

    feat: new version

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,32 +5,16 @@ import postcss from 'rollup-plugin-postcss';
 
 export default {
   input: 'src/components/index.ts',
-
   output: [
-    {
-      file: 'dist/index.js',
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      file: 'dist/index.esm.js',
-      format: 'esm',
-      sourcemap: true,
-    },
+    { file: 'dist/index.js', format: 'cjs', sourcemap: true },
+    { file: 'dist/index.esm.js', format: 'esm', sourcemap: true },
   ],
-
-  // 🔥 AQUÍ ESTÁ LA MAGIA
-  external: ['react', 'react-dom', 'react/jsx-runtime'],
 
   plugins: [
     resolve(),
     commonjs(),
-    postcss({
-      extract: true,
-    }),
-    typescript({
-      tsconfig: './tsconfig.json',
-      useTsconfigDeclarationDir: true,
-    }),
+    typescript({ useTsconfigDeclarationDir: true }),
+    postcss({ extract: true, minimize: true, use: ['sass'] }),
   ],
+  external: ['react', 'react-dom'],
 };
