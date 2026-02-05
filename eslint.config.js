697fa533db1250de881a1b5be0cae13ecc558bc5
MarshallRdgz <marshalltankian@gmail.com>
2026-02-05 00:33:01 -0600

    feat: new version

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,28 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { globals: globals.browser } },
   ...tseslint.configs.recommended,
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
+    // 🔧 FIX PARA EL WARNING DE REACT VERSION
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
     rules: {
-      "no-console": "off",
-      "no-debugger": "off",
-      "react/react-in-jsx-scope": "off",
-      "constructor-super": "off",
-      "no-undef": "warn",
-      "no-unused-vars": "warn",
+      'no-console': 'off',
+      'no-debugger': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'constructor-super': 'off',
+      'no-undef': 'warn',
+      'no-unused-vars': 'warn',
     },
   },
 ];
